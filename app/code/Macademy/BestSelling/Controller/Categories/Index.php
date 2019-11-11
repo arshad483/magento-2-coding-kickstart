15ca89a2675a58a9a6213327b05c4c718a475632
Mark Shust <mark@shust.com>
2019-11-11 13:08:50 -0500

    Create a controller action

---
+++ app/code/Macademy/BestSelling/Controller/Categories/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Macademy\BestSelling\Controller\Categories;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        die('Categories');
+    }
+}
---
+++ app/code/Macademy/BestSelling/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Macademy\BestSelling\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        die('Best Selling');
+    }
+}
