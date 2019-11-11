9bdac29b733b9b252583fe05fa995211b6127a39
Mark Shust <mark@shust.com>
2019-11-11 17:45:44 -0500

    Forward and redirect responses

---
+++ app/code/Macademy/BestSelling/Controller/Categories/Everything.php
@@ -0,0 +1,14 @@
+<?php
+namespace Macademy\BestSelling\Controller\Categories;
+
+use Magento\Framework\App\Action\Action;
+
+class Everything extends Action
+{
+    public function execute()
+    {
+        return $this->_redirect('*/*', [
+            'limit' => 1000,
+        ]);
+    }
+}
