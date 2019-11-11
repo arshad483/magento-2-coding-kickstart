3246de48a12f86eb4e6dadb55031ea438a575f81
Mark Shust <mark@shust.com>
2019-11-11 13:39:54 -0500

    Access the request object

--- app/code/Macademy/BestSelling/Controller/Categories/Index.php
+++ app/code/Macademy/BestSelling/Controller/Categories/Index.php
@@ -3,13 +3,18 @@ namespace Macademy\BestSelling\Controller\Categories;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\HTTP\PhpEnvironment\Request;
 
 class Index extends Action
 {
     public function execute()
     {
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents('Categories');
+        /** @var Request $request */
+        $request = $this->getRequest();
+        $categoryId = $request->getParam('category_id');
+        $limit = $request->getParam('limit');
+        $result->setContents("category_id: $categoryId, limit: $limit");
         return $result;
     }
 }
