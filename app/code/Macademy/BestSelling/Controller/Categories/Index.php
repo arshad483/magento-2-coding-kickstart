1acb573ba85c222d62637a27dffd839f0ff4a621
Mark Shust <mark@shust.com>
2019-11-11 13:14:16 -0500

    Create a proper raw response

--- app/code/Macademy/BestSelling/Controller/Categories/Index.php
+++ app/code/Macademy/BestSelling/Controller/Categories/Index.php
@@ -2,11 +2,14 @@
 namespace Macademy\BestSelling\Controller\Categories;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
     public function execute()
     {
-        die('Categories');
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents('Categories');
+        return $result;
     }
 }
--- app/code/Macademy/BestSelling/Controller/Index/Index.php
+++ app/code/Macademy/BestSelling/Controller/Index/Index.php
@@ -2,11 +2,14 @@
 namespace Macademy\BestSelling\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
     public function execute()
     {
-        die('Best Selling');
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents('Index');
+        return $result;
     }
 }
