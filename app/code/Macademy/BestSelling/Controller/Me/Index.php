f5425c6483a1796d42536265d957b446a52d74b1
Mark Shust <mark@shust.com>
2019-11-22 10:44:24 -0500

    Create objects with Object Manager

---
+++ app/code/Macademy/BestSelling/Controller/Me/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace Macademy\BestSelling\Controller\Me;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $objectManager = ObjectManager::getInstance();
+        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
+        $customerId = $customerSession->getCustomerId();
+        $result->setContents("customer id: $customerId");
+        return $result;
+    }
+}
