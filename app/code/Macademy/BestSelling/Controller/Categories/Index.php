0db43a952057caebcfeb9f2f12462584f74a29f4
Mark Shust <mark@shust.com>
2019-11-24 13:14:08 -0500

    Instantiate a collection with a factory

--- app/code/Macademy/BestSelling/Controller/Categories/Index.php
+++ app/code/Macademy/BestSelling/Controller/Categories/Index.php
@@ -2,11 +2,24 @@
 namespace Macademy\BestSelling\Controller\Categories;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\HTTP\PhpEnvironment\Request;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection as BestsellersCollection;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as BestsellersCollectionFactory;
 
 class Index extends Action
 {
+    protected $bestsellersCollectionFactory;
+
+    public function __construct(
+        Context $context,
+        BestsellersCollectionFactory $bestsellersCollectionFactory
+    ) {
+        $this->bestsellersCollectionFactory = $bestsellersCollectionFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
@@ -14,6 +27,10 @@ class Index extends Action
         $request = $this->getRequest();
         $categoryId = $request->getParam('category_id');
         $limit = $request->getParam('limit');
+
+        /** @var $bestsellersCollection BestsellersCollection */
+        $bestsellersCollection = $this->bestsellersCollectionFactory->create();
+
         $result->setContents("category_id: $categoryId, limit: $limit");
         return $result;
     }
